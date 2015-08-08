d965659384e50170ef50aa8590f36a218c533476
xinhuang327 <xinhuang327@gmail.com>
2015-08-08 14:57:10 +0800

    1st commit

---
+++ TestCSharp6/TestCSharp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestCSharp6
+{
+	class Point
+	{
+		public int X { get; }
+
+		public int Y { get; }
+
+		public Point (int x, int y)
+		{
+			this.X = x;
+			this.Y = y;
+		}
+
+		public int Area () => X * Y;
+
+		public override string ToString () => $"[Point: X={X}, Y={Y}]";
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var p = new Point (3, 4);
+			Console.WriteLine (p + " area: " + p.Area ());
+		}
+	}
+}
