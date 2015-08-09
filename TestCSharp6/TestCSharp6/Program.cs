61861846e80577d7040c227d87c19d25f56fdd79
xinhuang327 <xinhuang327@gmail.com>
2015-08-09 23:47:23 +0800

    * data.txt: resource operations.
    * res.resx:
    * Program.cs:
    * res.Designer.cs:
    * TestCSharp6.csproj:

--- TestCSharp6/TestCSharp6/Program.cs
+++ TestCSharp6/TestCSharp6/Program.cs
@@ -1,30 +1,45 @@
 ﻿using System;
+using System.Reflection;
+using System.IO;
+using System.Resources;
 
 namespace TestCSharp6
 {
-	class Point
-	{
-		public int X { get; }
+    class Point
+    {
+        public int X { get; }
 
-		public int Y { get; }
+        public int Y { get; }
 
-		public Point (int x, int y)
-		{
-			this.X = x;
-			this.Y = y;
-		}
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
 
-		public int Area () => X * Y;
+        public int Area() => X * Y;
 
-		public override string ToString () => $"[Point: X={X}, Y={Y}]";
-	}
+        public override string ToString() => $"[Point: X={X}, Y={Y}]";
+    }
 
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			var p = new Point (3, 4);
-			Console.WriteLine (p + " area: " + p.Area ());
-		}
-	}
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var p = new Point(3, 4);
+            Console.WriteLine(p + " area: " + p.Area());
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "TestCSharp6.data.txt";
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine(res.ResourceManager.GetString("NotesLabel"));
+        }
+    }
 }
